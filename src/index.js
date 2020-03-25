003cc0ce6c9df012f34e606378df44f120c2872f
Julian Pistorius <julian@julianpistorius.com>
2020-03-25 15:16:58 -0700

    Using `npm install` to manage PatternFly dependency.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './patternfly/patternfly.css';
+import '../node_modules/@patternfly/patternfly/patternfly.css';
 import './main.css';
 import { Elm } from './Main.elm';
 import * as serviceWorker from './serviceWorker';
