2ae371b3a165bb7013ec4941f052317b22ef5691
Julian Pistorius <julian@julianpistorius.com>
2020-03-25 14:58:44 -0700

    Moved copy-patternfly-assets.sh script to src directory.

---
+++ src/copy-patternfly-assets.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# TODO: Use `npm install --save-dev @patternfly/patternfly`
+
+mkdir -p ./patternfly
+
+cp /Users/julianp/dev/elm/spikes/patternfly-elements/node_modules/@patternfly/patternfly/patternfly.css ./patternfly
+cp -R /Users/julianp/dev/elm/spikes/patternfly-elements/node_modules/@patternfly/patternfly/assets ./patternfly
--- src/patternfly/copy-patternfly-assets.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# TODO: Use `npm install --save-dev @patternfly/patternfly`
-
-cp /Users/julianp/dev/elm/spikes/patternfly-elements/node_modules/@patternfly/patternfly/patternfly.css .
-cp -R /Users/julianp/dev/elm/spikes/patternfly-elements/node_modules/@patternfly/patternfly/assets .
